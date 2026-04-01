baedb30ad1d5f19d7dd3102d46394a40505a3fd6
kuularalbina <168761122+kuularalbina@users.noreply.github.com>
2026-04-01 11:38:42 +0700

    Delete Практическая_работа6 directory

--- Практическая_работа6/lab6/filelib.c
+++
@@ -1,8 +0,0 @@
-#include <unistd.h>
-#include <fcntl.h>
-
-void save_message(const char *filename, const char *message){
-    int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    write(fd, message, 7);
-    close(fd);
-}
--- Практическая_работа6/lab6/filelib.h
+++
@@ -1,6 +0,0 @@
-#ifndef FILELIB_H
-#define FILELIB_H
-
-void save_message(const char *filename, const char *message);
-
-#endif
